3850354c1882da7baeefe1a7704800d57da248db
pontchita28 <120283336+pontchita28@users.noreply.github.com>
2024-09-05 18:02:10 -0400

    Réponse aux questions complétées

--- hello.py
+++ hello.py
@@ -2,4 +2,9 @@ import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
 
+aire = math.pi * rayon ** 2 
+
+print(aire)
+
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Nom: Michel, Prénom: Soeuchelle")
